a4f62e4b32655bdc386c3f293134bec5396f0bc0
donchezdonta <minh.nln2421619@sis.hust.edu.vn>
2026-05-09 16:03:03 +0700

    Created a separate BlockView class for block render logic, allowing more flexibility in changing how blocks render in the future. Updated class diagram as class_diagram_v2

--- src/test/Main.java
+++ src/test/Main.java
@@ -2,8 +2,11 @@ package test;
 
 import model.block.BlockModel;
 import model.world.WorldModel;
+import view.BlockView;
 
 public class Main {
+    private static BlockView blockView = new BlockView();
+
     public static void main(String[] args) {
         // initialize 40x20 matrix
         int width = 40;
@@ -24,34 +27,8 @@ public class Main {
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
                 BlockModel block = grid[x][y];
-
-                // in case grid wasn't fully populated
-                if (block == null) {
-                    System.out.print("? ");
-                    continue;
-                }
-
-                // map block type string to a specific console character
-                String type = block.getBlockType();
-                char symbol = ' ';
-
-                switch (type) {
-                    case "water":
-                        symbol = '~'; // blue water
-                        break;
-                    case "dirt":
-                        symbol = '.'; // plain dirt
-                        break;
-                    case "grass":
-                        symbol = ','; // Scattered grass
-                        break;
-                    case "wood":
-                        symbol = 'T'; // tree/forest Blobs
-                        break;
-                    default:
-                        symbol = '?'; // unknown block for later testing
-                }
-                System.out.print(symbol + " ");
+                // block render
+                System.out.print(blockView.getBlockDisplay(block) + " ");
             }
 
             System.out.println();
