3832dce6905b6a0dee3e5888305ece2871da56b9
luongleafer <luongleafer2339@gmail.com>
2026-05-12 14:34:06 +0700

    Make world and UI updatable every frame

--- src/test/TestFX.java
+++ src/test/TestFX.java
@@ -1,11 +1,15 @@
 package test;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.concurrent.ScheduledService;
+import javafx.concurrent.Task;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import model.block.BlockModel;
 import model.world.WorldModel;
 import view.BlockView;
@@ -16,22 +20,68 @@ public class TestFX extends Application {
 
     WorldModel world = new WorldModel(worldWidth, worldHeight);
     BlockView blockView = new BlockView();
+    long tps = 20;
 
     public static void main(String[] args) {
         launch(args);
     }
 
+    long ticks;
+
+    ScheduledService<Integer> tickUpdateService = new ScheduledService() {
+
+        @Override
+        protected Task createTask() {
+            return new Task<Object>(){
+
+                @Override
+                protected Object call() throws Exception {
+                    ticks++;
+                    return null;
+                }
+            };
+        }
+    };
+
+    ScheduledService<Integer> updateWorldService = new ScheduledService() {
+        @Override
+        protected Task createTask() {
+            return new  Task<Object>(){
+                @Override
+                protected Object call() throws Exception {
+                    world.update();
+                    return null;
+                }
+            };
+        }
+    };
+
+    private Label countLabel = new Label("Counting...");
+    private Label textWorld = new Label();
+    AnimationTimer timer = new AnimationTimer() {
+        @Override
+        public void handle(long l) {
+           countLabel.setText("Tick Count: " + ticks);
+           textWorld.setText(worldView(worldWidth, worldHeight));
+        }
+    };
+
     @Override
     public void start(Stage stage) throws Exception {
+        tickUpdateService.setPeriod(Duration.seconds(1.0/tps));
+        tickUpdateService.start();
+        updateWorldService.setPeriod(Duration.seconds(1.0/tps));
+        updateWorldService.start();
         world.generateTerrain();
         String javaVersion = System.getProperty("java.version");
         String javafxVersion =  System.getProperty("javafx.version");
-        Label l = new Label(worldView(worldWidth, worldHeight));
-        l.setFont(Font.font("Roboto Mono"));
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+        textWorld.setFont(Font.font("Roboto Mono"));
+        Scene scene = new Scene(new VBox(textWorld, countLabel), 640, 480);
+
         stage.setTitle("Wild Life Simulation");
         stage.setScene(scene);
         stage.show();
+        timer.start();
     }
 
     private String worldView(int width, int height) {
