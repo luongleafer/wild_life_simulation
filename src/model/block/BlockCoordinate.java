2d6662d5c72c9faaf2b0dc248f541e6096dc5132
OOP.Lab.20252.202417050.Pham Thu Trang <phamthutrang.hust.17050@gmail.com>
2026-05-17 23:05:30 +0700

    Add BlockCoordinate.java to src/model/block independently

--- src/model/block/BlockCoordinate.java
+++ src/model/block/BlockCoordinate.java
@@ -1,18 +1,49 @@
 package model.block;
 
+import java.util.Objects;
+
 public class BlockCoordinate {
-    public int x;
-    public int y;
+    private int x;
+    private int y;
 
     public BlockCoordinate(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    // Getters and Setters
     public int getX() {
         return x;
     }
+    public void setX(int x) {
+        this.x = x;
+    } 
+    // Thêm setter nếu cần thay đổi tọa độ
+
     public int getY() {
         return y;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";       
+    }
+
+    @Override
+    // Thêm cái này để bảo vệ hàm equals 
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BlockCoordinate)) return false;
+        BlockCoordinate that = (BlockCoordinate) o;
+        return x == that.x && y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        // Tạo mã định danh duy nhất dựa trên cặp x và y
+        return Objects.hash(x, y);
     }
 }
