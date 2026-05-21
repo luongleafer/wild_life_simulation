b39dedb0c905ce2e5f2ca12e050c6d638e8402ed
Nguyen Tran Gia Huy <Huy.NTG2416942@sis.hust.edu.vn>
2026-05-21 18:08:20 +0700

    Replace String direction system with Enum

--- src/model/entity/AnimalModel.java
+++ src/model/entity/AnimalModel.java
@@ -15,8 +15,32 @@ public abstract class AnimalModel extends EntityModel {
     // but that is for later, when those behaviors are defined better
     private final String survivalStrategy;
     // direction may mean that this animal is chasing/fleeing from other entities.
-    // Possible acceptable keywords: north, south, east, west???
-    private final String direction;
+    // new method: use enums for direction
+    private enum Direction {
+        // Coords start from top left corner, not Cartesian
+        NORTH(0, -1),
+        SOUTH(0, 1),
+        EAST(1, 0),
+        WEST(-1, 0);
+
+        private final double dx;
+        private final double dy;
+
+        Direction(double dx, double dy) {
+            this.dx = dx;
+            this.dy = dy;
+        }
+
+        public double getDx() {
+            return dx;
+        }
+
+        public double getDy() {
+            return dy;
+        }
+    }
+
+    private final Direction direction;
 
     // Main methods
     // eat(food) food can be other Entity or Block, depend on the specific implementation of the animal
@@ -36,39 +60,25 @@ public abstract class AnimalModel extends EntityModel {
         this.energy += block.getEnergyValue();
     }
 
-    // Move in the current specified direction for some number of steps
-    private static final Random moveSteps = new Random();
-    public void move() {
-        if (direction == null) {
-            return;
-        }
-        // Move a random number of steps from 1-5 blocks
-        int steps = moveSteps.nextInt(5) + 1;
-        switch (direction.toLowerCase()) {
-            case "north":
-                move(getPosition().getPosX(), getPosition().getPosY() - steps);
-                break;
-            case "south":
-                move(getPosition().getPosX(), getPosition().getPosY() + steps);
-                break;
-            case "east":
-                move(getPosition().getPosX() + steps, getPosition().getPosY());
-                break;
-            case "west":
-                move(getPosition().getPosX() - steps, getPosition().getPosY());
-                break;
-            default:
-                return;
-        }
-    }
-
     // Move to specified coordinate, basic implementation
     public void move(double x, double y) {
         getPosition().setPosX(x);
         getPosition().setPosY(y);
     }
 
-    public AnimalModel(EntityCoordinate position, int health, int age, int adultAge, int oldAge, int totalLifespan, int currentState, float hunger, float thirst, float energy, String survivalStrategy, String direction) {
+    // Move in the current specified direction for some number of steps
+    private static final Random moveSteps = new Random();
+    public void move() {
+        // Move a random number of blocks from 1-5 blocks
+        double steps = moveSteps.nextDouble(5) + 1;
+
+        double newX = getPosition().getPosX() + direction.getDx() * steps;
+        double newY = getPosition().getPosY() + direction.getDy() * steps;
+
+        move(newX, newY);
+    }
+
+    public AnimalModel(EntityCoordinate position, int health, int age, int adultAge, int oldAge, int totalLifespan, int currentState, float hunger, float thirst, float energy, String survivalStrategy, Direction direction) {
         super(position, health, age, adultAge, oldAge, totalLifespan, currentState);
         this.hunger = hunger;
         this.thirst = thirst;
