906e2589f729f57472bfedf1fd039eaff886283b
Nguyễn Lê Nhật Minh <minh.nln2421619@sis.hust.edu.vn>
2026-05-21 22:27:21 +0700

    Apply suggestion from @donchezdonta
    tọa độ phải public thì class khác mới truy cập đc

--- src/model/block/BlockCoordinate.java
+++ src/model/block/BlockCoordinate.java
@@ -3,8 +3,8 @@ package model.block;
 import java.util.Objects;
 
 public class BlockCoordinate {
-    private int x;
-    private int y;
+    public int x;
+    public int y;
 
     public BlockCoordinate(int x, int y) {
         this.x = x;
