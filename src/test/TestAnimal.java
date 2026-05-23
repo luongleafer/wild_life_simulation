95761f55a9e609e403d12e400712235503067e3b
donchezdonta <minh.nln2421619@sis.hust.edu.vn>
2026-05-23 23:46:05 +0700

    #21: made a few changes to AnimalModel to accommodate animal movement simulation. Added TestAnimal and MovementCliDemo for testing.

---
+++ src/test/TestAnimal.java
@@ -0,0 +1,30 @@
+package test;
+
+import model.block.BlockModel;
+import model.entity.AnimalModel;
+import model.entity.EntityCoordinate;
+import model.entity.EntityModel;
+
+public class TestAnimal extends AnimalModel {
+    private final String name;
+
+    public TestAnimal(String name, EntityCoordinate position, double speed, double directionX, double directionY) {
+        super(position, 10, 0, 0, 0, 0, 0, 0.0f, 0.0f, 0.0f, "", speed, directionX, directionY);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void Interact(BlockModel block) {
+        // No-op for CLI demo.
+    }
+
+    @Override
+    public void Interact(EntityModel entity) {
+        // No-op for CLI demo.
+    }
+}
+
