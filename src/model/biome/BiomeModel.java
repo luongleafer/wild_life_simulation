92aeb3257ee4ea4b24d3b78faa4c7b3ae653a524
donchezdonta <minh.nln2421619@sis.hust.edu.vn>
2026-06-11 21:55:07 +0700

    add biome

--- src/model/biome/BiomeModel.java
+++ src/model/biome/BiomeModel.java
@@ -1,43 +1,16 @@
 package model.biome;
 
-import model.block.BlockCoordinate;
 import model.block.BlockModel;
-import model.block.BlockFactory;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Random;
 
 public abstract class BiomeModel {
-    protected List<BlockModel> blockPalette;
-    protected BlockCoordinate topLeft;
-    protected BlockCoordinate bottomRight;
+    public abstract BiomeType getBiomeType();
 
-    public BiomeModel(BlockCoordinate topLeft, BlockCoordinate bottomRight) {
-        this.topLeft = topLeft;
-        this.bottomRight = bottomRight;
-        this.blockPalette = new ArrayList<>();
-    }
-
-    // Returns a 1D array of all blocks generated within this biome's rectangle
-    public abstract BlockModel[] generate();
-
-    // Helper methods to get dimensions
-    protected int getWidth() {
-        return bottomRight.x - topLeft.x;
-    }
-
-    protected int getHeight() {
-        return bottomRight.y - topLeft.y;
-    }
-
-    protected List<BlockModel> generateBaseLayer(){
-        List<BlockModel> blocks = new ArrayList<>();
-        BlockModel baseBlock = blockPalette.getFirst();
-        for(int x = topLeft.x; x <  bottomRight.x; x++){
-            for(int y =  topLeft.y; y <  bottomRight.y; y++){
-                blocks.add(BlockFactory.create(baseBlock.getBlockType(), x, y));
-            }
-        }
-        return blocks;
-    }
+    public abstract BlockModel createBlock(int xPos,
+                                           int yPos,
+                                           float elevation,
+                                           float moisture,
+                                           boolean shallowWater,
+                                           Random random);
 }
