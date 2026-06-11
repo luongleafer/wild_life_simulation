9db9eb4f96281777565d22052f3204d6756bae25
LeNguyenKhanhLinh <lenguyenkhanhlinh.work@gmail.com>
2026-06-12 06:29:26 +0700

    tạo rừng

--- src/model/biome/BiomeModel.java
+++ src/model/biome/BiomeModel.java
@@ -7,10 +7,12 @@ import java.util.Random;
 public abstract class BiomeModel {
     public abstract BiomeType getBiomeType();
 
-    public abstract BlockModel createBlock(int xPos,
-                                           int yPos,
-                                           float elevation,
-                                           float moisture,
-                                           boolean shallowWater,
-                                           Random random);
+    public abstract BlockModel createBlock(        
+    		int xPos,
+            int yPos,
+            float elevation,
+            float moisture,
+            float forestDensity,
+            boolean shallowWater,
+            Random random);
 }
